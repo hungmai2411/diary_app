69e647e0ecd02009419b095cb3ed262f0bf1ca24
hungmai2411 <maiphamquochung@gmail.com>
2022-11-22 09:29:30 +0700

    up

--- lib/constants/app_assets.dart
+++ lib/constants/app_assets.dart
@@ -1,3 +1,9 @@
 class AppAssets {
-  static const String imagePath = 'assets/images/';
+  static const String imagePath = 'assets/icons/';
+  static const String iconAngry = 'assets/icons/angry.png';
+  static const String iconLoving = 'assets/icons/loving.png';
+  static const String iconSad = 'assets/icons/sad.png';
+  static const String iconScared = 'assets/icons/scared.png';
+  static const String iconSmile = 'assets/icons/smile.png';
+  static const String iconSurprised = 'assets/icons/surprised.png';
 }
